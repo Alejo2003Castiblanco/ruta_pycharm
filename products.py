ca8152c41915bf0062cbc33d9169f3c5e95237f2
Alejo2003Castiblanco <134987145+Alejo2003Castiblanco@users.noreply.github.com>
2023-08-15 16:11:59 -0500

    Delete products.py

--- products.py
+++
@@ -1,8 +0,0 @@
-#Creamos dos archivos uno llamado app.py y otro llamado products.py
-#Simulamos los datos creamos unos variable llamada products que tendra una lista de datos ingresados por nosotros mismos
-
-products = [
-    {"name": "laptop", "price": 800, "quantity": 4},
-    {"name": "mouse", "price": 40, "quantity": 10},
-    {"name": "monitor", "price": 400, "quantity": 3}
-]
